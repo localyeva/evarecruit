03654b22d384ab3132ba1a8378043a35b8e35456
Kantoh <leduongkhang@gmail.com>
2015-08-20 16:59:20 +0700

    Jobs Plugin

--- public_html/wp-content/plugins/jobs-management/jobs_management.php
+++ public_html/wp-content/plugins/jobs-management/jobs_management.php
@@ -16,23 +16,122 @@ if (!defined('ABSPATH')) {
     die('No script kiddies please!');
 }
 
-class jobs {
+class jobs_management {
 
     protected $pluginPath;
     protected $pluginUrl;
 
+    /**
+     * A Unique Identifier
+     */
+    protected $plugin_slug = 'jobs-management-plugin';
+
+    /**
+     * A reference to an instance of this class.
+     */
+    private static $instance;
+
+    /**
+     * The array of templates that this plugin tracks.
+     */
+    protected $templates;
+
+    /**
+     *
+     */
+    protected $widget_slug = 'jobs-management-widget';
+
+    public static function get_instance() {
+        if (null == self::$instance) {
+            self::$instance = new jobs_management();
+        }
+        return self::$instance;
+    }
+
     public function __construct() {
 
-        // set Plugin Path
-        $this->pluginPath = dirname(__FILE__);
+        $this->templates = array();
 
-        // Set plugin Url
-        $this->pluginUrl = WP_PLUGIN_URL . '/jobs';
+        // Add a filter to the attributes metabox to inject template into the cache.
+        add_filter('page_attributes_dropdown_pages_args', array($this, 'register_project_templates'));
+
+        // Add a filter to the save post to inject out template into the page cache
+        add_filter('wp_insert_post_data', array($this, 'register_project_templates'));
+
+        // Add a filter to the template include to determine if the page has our 
+        // template assigned and return it's path
+        add_filter('template_include', array($this, 'view_project_template'));
+
+        // Add your templates to this array.
+        $this->templates = array(
+            'templates/jobs.php' => 'Jobs',
+        );
+
 
 //        register_activation_hook(__FILE__, array($this, ''));
         add_action('init', array($this, 'cptui_register_my_cpts'));
         add_action('init', array($this, 'cptui_register_my_taxes'));
+        add_action('init', array($this, 'getAdminOptions'));
+
+
+        // --------------- test
+        add_action('init', array($this, 'test_template'));
+
         add_action('init', array($this, 'my_register_field_group'));
+
+        add_filter('get_sub_field', array($this, 'test_uppercase'));
+    }
+
+    public function test_uppercase($text = '') {
+        if (get_field('main_locations')) {
+            while (has_sub_field('repeater')) {
+//                $text = apply_filters('address', strtoupper($text));
+                $text = '11111111111111111111111' . get_sub_field('address');
+                echo $text . '<br>';
+            }
+        }
+
+        return $text;
+    }
+
+    var $adminOptionsName = "DevloungePluginSeriesAdminOptions";
+
+    function getAdminOptions() {
+        $devloungeAdminOptions = array('show_header' => 'true',
+            'add_content' => 'true',
+            'comment_author' => 'true',
+            'content' => '');
+        $devOptions = get_option($this->adminOptionsName);
+        if (!empty($devOptions)) {
+            foreach ($devOptions as $key => $option)
+                $devloungeAdminOptions[$key] = $option;
+        }
+        update_option($this->adminOptionsName, $devloungeAdminOptions);
+        return $devloungeAdminOptions;
+    }
+
+    function test_template() {
+        return get_template_part('/templates/jobs-form-list', 'page');
+    }
+
+    public function get_plugin_slug() {
+        return $this->plugin_slug;
+    }
+
+    public function get_plugin_path() {
+        // set Plugin Path
+        $this->pluginPath = plugin_dir_path(__FILE__);
+        return $this->pluginPath;
+    }
+
+    public function get_plugin_url() {
+        // Set plugin Url
+        $this->pluginUrl = WP_PLUGIN_URL . '/' . $this->get_plugin_slug();
+        return $this->pluginUrl;
+    }
+
+    public function get_widget_slug() {
+        return $this->widget_slug;
     }
 
     public function cptui_register_my_cpts() {
@@ -55,7 +154,7 @@ class jobs {
             "rewrite" => array("slug" => "job", "with_front" => true),
             "query_var" => true,
             "menu_position" => 28,
-            "menu_icon" => get_template_directory_uri() . '/img/ad-ico/h3.png',
+            "menu_icon" => plugins_url('images/ad-ico/h3.png', __FILE__),
             "supports" => array("title"),
         );
         register_post_type("job", $args);
@@ -201,6 +300,57 @@ class jobs {
         }
     }
 
+    public function register_project_templates($atts) {
+        // Create the key used for the themes cache
+        $cache_key = 'page_templates-' . md5(get_theme_root() . '/' . get_stylesheet());
+
+        // Retrieve the cache list. 
+        // If it doesn't exist, or it's empty prepare an array
+        $templates = wp_get_theme()->get_page_templates();
+        if (empty($templates)) {
+            $templates = array();
+        }
+
+        // New cache, therefore remove the old one
+        wp_cache_delete($cache_key, 'themes');
+
+        // Now add our template to the list of templates by merging our templates
+        // with the existing templates array from the cache.
+        $templates = array_merge($templates, $this->templates);
+
+        // Add the modified cache to allow WordPress to pick it up for listing
+        // available templates
+        wp_cache_add($cache_key, $templates, 'themes', 1800);
+
+        return $atts;
+    }
+
+    /**
+     * Checks if the template is assigned to the page
+     */
+    public function view_project_template($template) {
+
+        global $post;
+
+        if (!isset($this->templates[get_post_meta($post->ID, '_wp_page_template', true)])) {
+
+            return $template;
+        }
+
+        $file = $this->get_plugin_path() . get_post_meta($post->ID, '_wp_page_template', true);
+
+        // Just to be safe, we check if the file exist first
+        if (file_exists($file)) {
+            return $file;
+        } else {
+            echo $file;
+        }
+
+        return $template;
+    }
+
 }
 
-$jobs = new jobs();
+//add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
+
+$jobs_management = new jobs_management();
---
+++ public_html/wp-content/plugins/jobs-management/templates/jobs.php
@@ -0,0 +1,230 @@
+<?php
+/*
+ * Author: KhangLe
+ * Template Name: Jobs
+ * 
+ */
+
+if (!defined('ABSPATH')) {
+    die('No script kiddies please!');
+}
+
+//get_header();
+?>
+
+<!--//Search Bar-->
+<div class="header-search-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <div class="col-xs-12 col-md-6">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Nhập chức danh, ngành nghề, từ khóa">
+                        </div>
+                        <div class="col-xs-12 col-xs-3">
+                            <select name="position" class="form-control">
+                                <option value="">Vị trí</option>
+                            </select>
+                        </div>
+                        <div class="col-xs-12 col-xs-3">
+                            <select name="position" class="form-control">
+                                <option value="">Địa điểm</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+                <div class="row-gap-medium"></div>
+                <div class="row-gap-small"></div>
+                <div class="row">
+                    <div class="col-xs-12 col-md-10">
+                        <p class="stat">BPO (2) <span class="vertical-bar">|</span> Communicator (2) <span class="vertical-bar">|</span> BSE (1) <span class="vertical-bar">|</span> Developer (11)</p>
+                    </div>
+                    <div class="col-xs-12 col-md-2 pull-right">
+                        <button class="btn btn-block btn-search btn-orange"><i class="glyphicon glyphicon-menu-right"></i><i class="glyphicon glyphicon-menu-right"></i> Tìm kiếm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//Search Bar End-->
+
+<!--//Jobs List-->
+<div class="container header-job-list">
+    <!-- hot -->
+    <div class="row item hot">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item hot">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item hot">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+    <!-- lastest -->
+    <div class="row item">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+
+    <div class="row item">
+        <div class="col-xs-6">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+            <div class="info">
+                <span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="text-blue">Internation Skill</div>
+            <div class="text-blue">Technical Field Knowledge</div>
+            <div class="text-blue">Management & Leadership</div>
+        </div>
+        <div class="col-xs-3">
+            <div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+            <div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+        </div>
+    </div>
+</div>
+<!--//Jobs List End-->
+
+<!--//Paging-->
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 no-padding-l">
+            <nav>
+                <ul class="pagination">
+                    <li>
+                        <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">Prev</span>
+                        </a>
+                    </li>
+                    <li><a href="#" class="active">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="Next">
+                            <span aria-hidden="true">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<!--//Paging End-->
+
+<?php // get_footer(); ?>
