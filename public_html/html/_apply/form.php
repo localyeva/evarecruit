625c8c1568214ca7da4f1ac0f44ad59632192014
Tran Khanh <khanhth.evolableasia@gmail.com>
2015-08-21 16:49:18 +0700

    Hotfix

---
+++ public_html/html/_apply/form.php
@@ -0,0 +1,85 @@
+<div class="row-gap-large"></div>
+<div class="row">
+	<div class="col-xs-12 col-md-8 col-md-offset-2">
+		<form class="form-horizontal">
+		  	<div class="form-group">
+			    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+			    <div class="col-sm-10">
+					<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+			    </div>
+		  	</div>
+
+	  	  	<div class="form-group">
+	  		    <label for="inputEmail3" class="col-sm-2 control-label">Full Name</label>
+	  		    <div class="col-sm-10">
+	  				<input type="Fullname" class="form-control" id="inputFullname3" placeholder="Full Name">
+	  		    </div>
+	  	  	</div>
+
+  	  	  	<div class="form-group">
+  	  		    <label for="inputEmail3" class="col-sm-2 control-label">Phone Number</label>
+  	  		    <div class="col-sm-10">
+  	  				<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+  	  		    </div>
+  	  	  	</div>
+  	  	  	<div class="form-group">
+				<label for="inputEmail3" class="col-sm-2 control-label">Attach CV</label>
+				<div class="col-sm-10">
+		  	  	  	<div class="radio">
+		  	  	  		<label>
+		  	  	  	    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+		  	  	  	    	Option two can be something else and selecting it will deselect option one
+		  	  	  		</label>
+		  	  	  	</div>
+	  	  	  	</div>
+  	  	  	</div>
+
+  	  	  	<div class="form-group">
+  	  		    <label for="inputEmail3" class="col-sm-2 control-label">Attach CV</label>
+  	  		    <div class="col-sm-10">
+  	  				<button class="btn btn-blue">Browse</button> No file selected
+  	  		    </div>
+  	  	  	</div>
+
+			<div class="row-gap-medium"></div>
+  	  		<div class="box nano">
+  	  	  		<div class="row">
+  	  	  			<div class="col-xs-10">
+  	  	  				<p class="nano-content">
+	  	  	  				<span class="text-bold">EVOLABLE ASIA Co., Ltd</span> consectetur adipisicing elit, sed do eiusmod
+	  	  	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  	  	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  	  	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  	  	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  	  	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  	  	  				</p>
+  	  	  			</div>
+  	  	  		</div>
+  	  	  	</div>
+  	  	  	<div class="row-gap-small"></div>
+			<div class="row">
+				<div class="col-xs-12">
+		  	  	  	<div class="checkbox">
+		  	  	  	  <label>
+		  	  	  	    <input type="checkbox" value="" checked>
+		  	  	  	    Option one is this and that&mdash;be sure to include why it's great
+		  	  	  	  </label>
+		  	  	  	</div>
+				</div>
+			</div>
+			<div class="row-gap-medium"></div>
+			<div class="row">
+				<div class="col-xs-3 col-xs-offset-9">
+					<button class="btn btn-block btn-orange">Apply</button>
+				</div>
+			</div>
+			<div class="row-gap-large"></div>
+		</form>
+	</div>
+</div>
+<script type="text/javascript" src="js/jquery.nanoscroller.min.js"></script>
+<script type="text/javascript">
+	$(function () {
+		$(".nano").nanoScroller();
+	});
+</script>
---
+++ public_html/html/_apply/header.php
@@ -0,0 +1,5 @@
+<div class="header-top-apply">
+	<div class="container text-center">
+		<h2 class="text-bold">Apply Your Resume</h2>
+	</div>
+</div>
