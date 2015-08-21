9ef3634768ec18490d37ca19f06aa4e78e99f24c
Tran Khanh <khanhth.evolableasia@gmail.com>
2015-08-21 15:01:01 +0700

    Hotfix

--- public_html/html/_new_jobs/search_bar.php
+++ public_html/html/_new_jobs/search_bar.php
@@ -23,7 +23,7 @@
 				<div class="row-gap-small"></div>
 				<div class="row">
 					<div class="col-xs-12 col-md-10">
-						<p class="stat">BPO (2) <span class="vertical-bar">|</span> Communicator (2) <span class="vertical-bar">|</span> BSE (1) <span class="vertical-bar">|</span> Developer (11)</p>
+						<p class="stat"><a href="#" class="white-link">BPO (2)</a> <span class="vertical-bar">|</span> <a href="#" class="white-link">Communicator (2)</a> <span class="vertical-bar">|</span> <a href="#" class="white-link">BSE (1)</a> <span class="vertical-bar">|</span> Developer (11)</p>
 					</div>
 					<div class="col-xs-12 col-md-2 pull-right">
 						<button class="btn btn-block btn-search btn-orange"><i class="glyphicon glyphicon-menu-right"></i><i class="glyphicon glyphicon-menu-right"></i> Tìm kiếm</button>
