7f32467a0ba9dd80984b003460a7eea51ba486d5
Tran Khanh <khanhth.evolableasia@gmail.com>
2015-08-25 13:29:37 +0700

    Hotfix

--- public_html/html/_home/job_list.php
+++ public_html/html/_home/job_list.php
@@ -1,4 +1,4 @@
-<div class="header-job-list">
+<div class="header-job-list home-page">
 	<div class="container">
 		<div class="row-gap-medium"></div>
 		<h2 class="text-center">Find your dream jobs</h2>
@@ -7,140 +7,193 @@
 			<div class="col-xs-12 block-center">
 				<form class="form-horizontal">
 					<div class="form-group">
-						<div class="col-xs-7 col-md-9">
+						<div class="col-xs-12 col-md-6">
 							<input type="email" class="form-control" id="inputEmail3" placeholder="Nhập chức danh, ngành nghề, từ khóa">
 						</div>
-						<div class="col-xs-offset-1 col-xs-4 col-md-offset-1 col-md-2">
-							<button class="btn btn-block btn-search"><i class="glyphicon glyphicon-menu-right"></i><i class="glyphicon glyphicon-menu-right"></i> Tìm kiếm</button>
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-3">
+						<div class="col-xs-12 col-xs-3">
 							<select name="position" class="form-control">
 								<option value="">Vị trí</option>
 							</select>
 						</div>
-						<div class="col-xs-3">
+						<div class="col-xs-12 col-xs-3">
 							<select name="position" class="form-control">
 								<option value="">Địa điểm</option>
 							</select>
 						</div>
 					</div>
 				</form>
+				<div class="row-gap-medium"></div>
+				<div class="row-gap-small"></div>
 				<div class="row">
-					<div class="col-xs-12">
-						BPO (2) | Communicator (2) | BSE (1) | Developer (11)
+					<div class="col-xs-12 col-md-10">
+						<p class="stat"><a href="#" class="white-link text-bold">BPO (2)</a> <span class="vertical-bar">|</span> <a href="#" class="white-link text-bold">Communicator (2)</a> <span class="vertical-bar">|</span> <a href="#" class="white-link text-bold">BSE (1)</a> <span class="vertical-bar">|</span> <a href="#" class="white-link text-bold">Developer (11)</a></p>
+					</div>
+					<div class="col-xs-12 col-md-2 pull-right">
+						<button class="btn btn-block btn-search btn-orange"><i class="glyphicon glyphicon-menu-right"></i><i class="glyphicon glyphicon-menu-right"></i> Tìm kiếm</button>
 					</div>
 				</div>
 				<div class="row-gap-small"></div>
 				<!-- list of jobs -->
-				<div class="row">
-					<div class="col-xs-12">
-						<div class="item-list nano">
-							<div class="nano-content">
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+				<div class="list">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="nano">
+								<div class="nano-content">
+									<!-- hot -->
+									<div class="row item hot">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item hot">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item hot">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+									<!-- lastest -->
+									<div class="row item">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-								</div>
-								<div class="row item">
-									<div class="col-xs-1 status">NEW</div>
-									<div class="col-xs-6">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
-									</div>
-									<div class="col-xs-5">
-										<div class="title">Factory Director - Secretary</div>
-										<div class="text-muted">04 AUG 2015 TP.HCM</div>
+
+									<div class="row item">
+										<div class="col-xs-6">
+											<div class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+											<div class="info">
+												<span class="localtion">Ho Chi Minh</span> | <span class="level">Team Leader/Supervisor</span>
+											</div>
+										</div>
+										<div class="col-xs-3">
+											<div class="text-blue">Internation Skill</div>
+											<div class="text-blue">Technical Field Knowledge</div>
+											<div class="text-blue">Management & Leadership</div>
+										</div>
+										<div class="col-xs-3">
+											<div><img class="date" src="img/new_job/4.png" alt=""> Posted: 13/08/2015</div>
+											<div><img class="view" src="img/new_job/5.png" alt=""> Views: 92</div>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<script type="text/javascript" src="js/jquery.nanoscroller.min.js"></script>
+				<script type="text/javascript">
+					$(function () {
+						$(".nano").nanoScroller();
+					});
+				</script>
 				<div class="row-gap-large"></div>
 				<!-- end of job list -->
 			</div>
