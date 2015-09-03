c08a6c60ca3c34efb4f9f541f218d4fc2258896e
Tran Khanh <khanhth.evolableasia@gmail.com>
2015-09-03 09:41:00 +0700

    Hotfix

---
+++ public_html/html/corporate/_include/explain.php
@@ -0,0 +1,21 @@
+<div class="header-explain">
+	<div class="contaner">
+		<div class="row">
+			<div class="col-xs-5">
+				<h1>Why is Evolable Asia chosen in Vietnam</h1>
+				<div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</div>
+			</div>
+			<div class="col-xs-7">
+				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+			</div>
+		</div>
+	</div>
+</div>
