f48043688c5d98f4ee24b81e52795f5445f4317f
Tran Khanh <khanhth@evolableasia.vn>
2015-09-22 14:52:32 +0700

    Hotfix

--- public_html/html/_home/banner.php
+++ public_html/html/_home/banner.php
@@ -17,22 +17,25 @@
 			<div class="item active">
 				<img data-src="holder.js/900x500/auto/#777:#777" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					<h1 class="text-bold standout">Welcome Evolable Asia</h1>
+					<h2 class="mission">Our mission</h2>
+					<h3 clas"out-tro">To be No.1 providing IT software development support in Asia</h3>
 				</div>
 			</div>
 			<div class="item">
 				<img data-src="holder.js/900x500/auto/#666:#666" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					<h1 class="text-bold standout">Welcome Evolable Asia</h1>
+					<h2 class="mission">Our mission</h2>
+					<h3 clas"out-tro">To be No.1 providing IT software development support in Asia</h3>
 				</div>
 			</div>
 			<div class="item">
 				<img data-src="holder.js/900x500/auto/#555:#555" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
-					<h3>Third slide label</h3>
-					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+					<h1 class="text-bold standout">Welcome Evolable Asia</h1>
+					<h2 class="mission">Our mission</h2>
+					<h3 clas"out-tro">To be No.1 providing IT software development support in Asia</h3>
 				</div>
 			</div>
 		</div>
