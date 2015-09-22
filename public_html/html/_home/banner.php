6731f5b571f6b886c1dddd84070752677c98a647
Tran Khanh <khanhth@evolableasia.vn>
2015-09-22 14:39:48 +0700

    Hotfix

--- public_html/html/_home/banner.php
+++ public_html/html/_home/banner.php
@@ -15,34 +15,34 @@
 		</ol>
 		<div class="carousel-inner" role="listbox">
 			<div class="item active">
-				<img data-src="holder.js/900x500/auto/#777:#777" alt="900x500" src="../img/1.png" data-holder-rendered="true">
+				<img data-src="holder.js/900x500/auto/#777:#777" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
 					<h3>First slide label</h3>
 					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
 				</div>
 			</div>
 			<div class="item">
-				<img data-src="holder.js/900x500/auto/#666:#666" alt="900x500" src="../img/1.png" data-holder-rendered="true">
+				<img data-src="holder.js/900x500/auto/#666:#666" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
 					<h3>Second slide label</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
 			</div>
 			<div class="item">
-				<img data-src="holder.js/900x500/auto/#555:#555" alt="900x500" src="../img/1.png" data-holder-rendered="true">
+				<img data-src="holder.js/900x500/auto/#555:#555" alt="900x500" src="img/1.png" data-holder-rendered="true">
 				<div class="carousel-caption">
 					<h3>Third slide label</h3>
 					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
 				</div>
 			</div>
 		</div>
-		<a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev">
+		<!-- <a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
 		<a class="right carousel-control" href="#carousel-example-captions" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
-		</a>
+		</a> -->
 	</div>
 </div>
