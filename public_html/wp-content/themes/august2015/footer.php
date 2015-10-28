9a709fc70f3ad34aff52c087a701b4ae0c88e550
Ly Vinh Truong <truonglv@evolableasia.vn>
2015-10-28 10:26:49 +0700

    Fix facebook like box style

--- public_html/wp-content/themes/august2015/footer.php
+++ public_html/wp-content/themes/august2015/footer.php
@@ -15,6 +15,7 @@
             <div class="col-md-6 col-xs-12">
                 <div class="facebook">
                     <div id="fb-root"></div>
+                    <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fevolable.asia.eva%3Ffref%3Dts&amp;width=500px&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=445550542212549" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100%; height:290px;" allowTransparency="true"></iframe>
                 </div>
             </div>
         </div>
