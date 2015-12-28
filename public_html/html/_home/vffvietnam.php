28fc9aba88004324b4ef8309185c71802781ebc5
baothi <baothi246@gmail.com>
2015-12-28 13:48:29 +0700

    test okey rui

--- public_html/html/_home/vffvietnam.php
+++ public_html/html/_home/vffvietnam.php
@@ -1,87 +1,76 @@
-<section id="vffvientnam" class="main">
+<section id="vffvientnam">
 
   <div class="container">
     <div class="row ">
-      <div class="col-md-1"></div>
-      <div class="col-md-10 text-margin col-xs-12">
-        <h3 class=" margin-size text-colorvff"> <b>Evolable Asia</b></h3><h3 class="text-color margin-size"> <b>là nhà tài trợ chính thức</b></h3>
+      <div class="col-md-1 col-xs-12 "></div>
+      <div class="col-md-10 col-xs-12  text-margin">
+        <h3 class=" text-colorvff margin-size"> <b>Evolable Asia</b></h3><h3 class="text-color margin-size"> <b>là nhà tài trợ chính thức</b></h3>
       </div>
-      <div class="col-md-1"></div>
+      <div class="col-md-1 col-xs-12 "></div>
     </div>
     <div class="row ">
-      <div class="col-md-2"></div>
-      <div class="col-md-8 col-xs-12">
+      <div class="col-md-2 col-xs-12 "></div>
+      <div class="col-md-8 col-xs-12 ">
         <h3 class="margin-sizevff text-colorvff">cho đội tuyển bóng đá Việt Nam</h3>
       </div>
-      <div class="col-md-2"></div>
+      <div class="col-md-2 col-xs-12 "></div>
     </div>
     <div class="row voffset1"></div>
     <div class="row">
-      <div class="col-md-2"></div>
-      <div class="col-md-8 col-xs-12">
-        <img src="img/vff/5.png" />
-      </div>
-      <div class="col-md-2"></div>
+      <img src="img/vff/5.png" class="img-responsive center-block " alt="Responsive Image"/>
     </div>
-  </div>
+    <div class="row voffset1"></div>
+    <div class="row voffset1"></div>
 
-  <div id="logo" class="" data-ride=""></div>
+    <div class="row">
+      <img src="img/vff/2.png" class="img-responsive center-block " alt="Responsive Image"/>
+    </div>
+    <div class="row voffset1"></div>
+    <div class="row voffset1"></div>
 
-  <div class="container">
     <div class="text-left text-colorvff">
       <div class="row">
         <div class="col-md-2 col-xs-12"></div>
         <div class="col-md-8 col-xs-12">
-          <div class="row">
-            <div class="col-xs-12">
-              <p>
-                Evolable Asia đã ký hợp đồng làm nhà tài trợ bóng đá Việt Nam vào ngày 5 tháng 8 năm 2015 (liên đoàn bóng đá Việt Nam)
-              </p>
-            </div>
-          </div>
+          <p>Evolable Asia đã ký hợp đồng làm nhà tài trợ bóng đá Việt Nam vào ngày 5 tháng 8 năm 2015 (liên đoàn bóng đá Việt Nam)</p>
         </div>
         <div class="col-md-2 col-xs-12"></div>
       </div>
 
       <div class="row">
-        <div class="col-md-2"></div>
+        <div class="col-md-2 col-xs-12"></div>
         <div class="col-md-8 col-xs-12">
-          <p>
-            Tôi nghĩ rằng công ty ký hợp đồng tài trợ mà không có vụ lợi cho mình như việc hướng về những người tiêu dùng là rất hiếm
-          </p>
+          <p>Tôi nghĩ rằng công ty ký hợp đồng tài trợ mà không có vụ lợi cho mình như việc hướng về những người tiêu dùng là rất hiếm</p>
         </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-2 col-xs-12"></div>
       </div>
 
       <div class="row">
-        <div class="col-md-2"></div>
+        <div class="col-md-2 col-xs-12"></div>
         <div class="col-md-8 col-xs-12">
-          <p>
-            Tôi xin phép được giới thiệu về lý do ký hợp đồng tài trợ lần này với lập trường tiến hành kinh doanh và được sự ủng hộ của nhiều khách hàng.
-          </p>
+          <p>Tôi xin phép được giới thiệu về lý do ký hợp đồng tài trợ lần này với lập trường tiến hành kinh doanh và được sự ủng hộ của nhiều khách hàng.</p>
         </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-2 col-xs-12"></div>
       </div>
-
     </div>
+
   </div>
 </section>
 
 <section>
   <div id="style1">
     <div class="container">
-      <!-- <div class="row voffset2"></div> -->
+      <div class="row voffset2"></div>
       <div class="row">
-        <div class="text-center classic col-xs-12">
-          <h2>  Việc muốn làm nhà tài trợ cho Việt Nam</h2>
-          <h2>  có 3 lý do</h2>
+        <div class="text-center classic">
+          <h2>Việc muốn làm nhà tài trợ cho Việt Nam có <br/> 3 lý do</h2>
         </div>
       </div>
     </div>
   </div>
 </section>
 <section>
-    <div id="" class="container" data-ride="">
+    <div class="container">
       <div class="text-center classic">
       <!-- dòng đầu tiên -->
         <div class="row voffset1"></div>
@@ -89,7 +78,7 @@
           <div class="col-md-2"></div>
           <div class="col-md-8 col-xs-12">
 
-              <img src="img/vff/6.png" class="text-colorimg" />
+              <img src="img/vff/6.png" class="text-colorimg" alt="Responsive Image"/>
               <h3 class="text-color"> <b>Tôi muốn cho mọi người biết</b></h3>
 
           </div>
@@ -100,7 +89,7 @@
 
         <div class="row">
           <div class="col-md-2"></div>
-          <div class="col-md-8 col-xs-12">
+          <div class="col-md-8">
               <h3 class="text-color text-size"><b>Công Ty Evolable Asia là công ty như thế nào</b><h3>
           </div>
           <div class="col-md-2"></div>
@@ -120,10 +109,8 @@
         <div class="text-left">
           <div class="row">
             <div class="col-md-2"></div>
-            <div class="col-md-8 col-xs-12">
-              <p>
-                Chúng tôi là công ty mới vừa được thành lập vào năm 2012
-              </p>
+            <div class="col-md-8">
+              <p>Chúng tôi là công ty mới vừa được thành lập vào năm 2012</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -131,9 +118,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Trong ngành IT cũng nhiều người tìm việc chưa biết công ty chúng tôi, họ cũng không biết công ty chúng tôi là công ty như thế nào
-              </p>
+              <p>Trong ngành IT cũng nhiều người tìm việc chưa biết công ty chúng tôi, họ cũng không biết công ty chúng tôi là công ty như thế nào.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -141,9 +126,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Tôi muốn nhận được sự cho phép tài trợ của VFF với suy nghĩ muốn truyền tải đơn giản "công ty chúng tôi là công ty như thế nào
-              </p>
+              <p>Tôi muốn nhận được sự cho phép tài trợ của VFF với suy nghĩ muốn truyền tải đơn giản "công ty chúng tôi là công ty như thế nào.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -151,9 +134,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Tóm lại, nhân cơ hội này có thể truyền tải cho người tìm việc hiểu rõ cảm giác và ấn tượng " công ty kinh doanh có thể tài trợ cho VFF một cách trong sáng:, "Công ty muốn phát triển cùng với Việt Nam","là một công ty đương đầu với thế giới thông qua kinh doanh phát triển hệ thống".
-              </p>
+              <p>Tóm lại, nhân cơ hội này có thể truyền tải cho người tìm việc hiểu rõ cảm giác và ấn tượng " công ty kinh doanh có thể tài trợ cho VFF một cách trong sáng:, "Công ty muốn phát triển cùng với Việt Nam","là một công ty đương đầu với thế giới thông qua kinh doanh phát triển hệ thống".</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -195,9 +176,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Ai cũng biết là nhân viên của chúng tôi hầu hết là người Việt Nam
-              </p>
+              <p>Ai cũng biết là nhân viên của chúng tôi hầu hết là người Việt Nam</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -205,9 +184,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Việc làm nhà tài trợ của VFF là niềm tự hào của người Việt Nam, niềm tự hào này chính là một phần lợi ích và thành quả nỗ lực của từng nhân viên.
-              </p>
+              <p>Việc làm nhà tài trợ của VFF là niềm tự hào của người Việt Nam, niềm tự hào này chính là một phần lợi ích và thành quả nỗ lực của từng nhân viên.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -215,9 +192,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Điều này có nghĩa là kết quả nỗ lực của từng người đã giúp chúng ta trở nên có sức để trưởng thành trong việc làm đại biểu Việt Nam.
-              </p>
+              <p>Điều này có nghĩa là kết quả nỗ lực của từng người đã giúp chúng ta trở nên có sức để trưởng thành trong việc làm đại biểu Việt Nam.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -225,9 +200,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Tôi nghĩ rằng chúng ta hãy giành chiến thắng, đoàn kết cùng VFF như Slogan trong công ty "Chúng ta chơi, chúng ta giành chiến thắng".
-              </p>
+              <p>Tôi nghĩ rằng chúng ta hãy giành chiến thắng, đoàn kết cùng VFF như Slogan trong công ty "Chúng ta chơi, chúng ta giành chiến thắng".</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -261,9 +234,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Việc làm nhà tài trợ của VFF chính là tuyên ngôn mang tính đối ngoại của việc kinh doanh đang trưởng thành cùng với Việt Nam.
-              </p>
+              <p>Việc làm nhà tài trợ của VFF chính là tuyên ngôn mang tính đối ngoại của việc kinh doanh đang trưởng thành cùng với Việt Nam.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -271,9 +242,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Thông qua việc áp đặt cho mình một trách nhiệm nặng nề là làm nhà tài trợ của VFF, biểu thị ý chí phát triển Việt Nam thông qua kinh doanh.
-              </p>
+              <p>Thông qua việc áp đặt cho mình một trách nhiệm nặng nề là làm nhà tài trợ của VFF, biểu thị ý chí phát triển Việt Nam thông qua kinh doanh.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -281,9 +250,7 @@
           <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-              <p>
-                Cùng với khách hàng và nhân viên, chúng tôi muốn đóng góp vào sự phát triển của Việt Nam.
-              </p>
+              <p>Cùng với khách hàng và nhân viên, chúng tôi muốn đóng góp vào sự phát triển của Việt Nam.</p>
             </div>
             <div class="col-md-2"></div>
           </div>
@@ -300,7 +267,7 @@
   <div id="container">
     <div id="content">
       <div id="box">
-        <div class="col-md-12">
+        <div class="col-md-12 col-xs-12">
           <div id="child" class="child"> <h2><b>Join us our team</b></h2></div>
         </div>
       </div>
