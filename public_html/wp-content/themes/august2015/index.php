9fd8dd270dd3c635482050f70dc5ce32cc53f015
baothi <baothi246@gmail.com>
2016-01-05 08:47:57 +0700

    lam nut VFF

--- public_html/wp-content/themes/august2015/index.php
+++ public_html/wp-content/themes/august2015/index.php
@@ -91,7 +91,8 @@ if ($loop->have_posts()) {
                 <?php wp_reset_postdata() ?>
                 <!-- baothi start 31/12/2015 -->
               <div class="row">
-                <a href="http://evarecruit.localhost/vff" class="btn submit_vff">VFF</a>
+                <a href="http://evarecruit.localhost/vff" class="btn submit_vff">
+                <i class="fa fa-arrow-circle-o-right fa-4 fa_vff"></i>VFF</a>
               </div>
               <!-- baothi start 31/12/2015 -->
             </div>
