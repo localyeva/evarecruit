bc9a8839969d9c94243f5575eee38773965cbcff
baothi <baothi246@gmail.com>
2016-01-07 10:06:55 +0700

    bo di nhung the div khong can thiet

--- public_html/wp-content/themes/august2015/page-vff.php
+++ public_html/wp-content/themes/august2015/page-vff.php
@@ -31,27 +31,21 @@ get_header();
 
         <div class="text-left text-colorvff voffset1">
             <div class="row">
-                <div class="col-md-2 col-xs-12"></div>
-                <div class="col-md-8 col-xs-12">
+                <div class="col-md-8 col-xs-12 col-md-offset-2">
                     <p>Vào ngày 5/8/2015, Evolable Asia đã chính thức ký hợp đồng trở thành nhà tài trợ cho liên đoàn bóng đá Việt Nam ( Vietnam football Federation, dưới đây gọi tắt là VFF).</p>
                 </div>
-                <div class="col-md-2 col-xs-12"></div>
             </div>
 
             <div class="row">
-                <div class="col-md-2 col-xs-12"></div>
-                <div class="col-md-8 col-xs-12">
+                <div class="col-md-8 col-xs-12 col-md-offset-2">
                     <p>Việc mà một công ty hoàn toàn không phải là công ty chuyên sản xuất và kinh doanh các mặt hàng tiêu dùng - như Evolable Asia trở thành nhà tài trợ cho liên đoàn bóng đá Việt Nam , là một hiện tượng hiếm.</p>
                 </div>
-                <div class="col-md-2 col-xs-12"></div>
             </div>
 
             <div class="row">
-                <div class="col-md-2 col-xs-12"></div>
-                <div class="col-md-8 col-xs-12">
+                <div class="col-md-8 col-xs-12 col-md-offset-2">
                     <p>Vậy tại sao Evolable Asia lại có một quyết định như vậy? </p>
                 </div>
-                <div class="col-md-2 col-xs-12"></div>
             </div>
         </div>
 
@@ -74,129 +68,101 @@ get_header();
     <div class="container">
         <div class="text-center classic">
             <div class="row voffset1">
-                <div class="col-md-2"></div>
-                <div class="col-md-8 col-xs-12">
+                <div class="col-md-8 col-xs-12 col-md-offset-2">
 
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/6.png" class="text-colorimg" alt="Responsive Image"/>
                     <h3 class="text-color"> <b>Đưa Evolable Asia đến gần với các ứng viên hơn.</b></h3>
 
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="row voffset3">
-                <div class="col-md-2"></div>
-                <div class="col-md-8 col-xs-12">
+                <div class="col-md-8 col-xs-12 col-md-offset-2">
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/5.png" />
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="text-left voffset3">
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Công ty chúng tôi được thành lập vào năm 2012, vẫn còn là một cái tên khá mới giữa các công ty IT khác trên thị trường.</p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Ngay cả các kĩ sư IT khi đi tìm việc , có không ít bạn sẽ thắc mắc rằng: công ty Evolable Asia là công ty như thế nào ? </p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Việc chúng tôi trở thành nhà tài trợ cho liên đoàn bóng đá Việt Nam cũng là để trả lời câu hỏi này cho các bạn. </p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Chúng tôi muốn trả lời với các bạn rằng : Công ty chúng tôi có nền tảng kinh doanh vững chắc có đủ tiêu chuẩn trở thành nhà tài trợ cho liên đoàn bóng đá Việt Nam; Chúng tôi sẽ trưởng thành và lớn mạnh cùng với Việt Nam ; Chúng tôi muốn thông qua ngành nghề kinh doanh chính của mình là "phát triển phần mềm" để cùng với đội tuyển Việt Nam "cạnh tranh với các đối thủ trên toàn Thế Giới".</p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Thông qua việc này , chúng tôi mong muốn các bạn - đặc biệt là những kỹ sư IT đang đi tìm việc hiểu rõ hơn về giá trị của công ty chúng tôi.</p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
             </div>
 
             <div class="row voffset3">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-md-offset-2">
 
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/7.png" class="text-colorimg" />
                     <h3 class="text-color"> <b> United we play. United we win.</b></h3>
 
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="row voffset3">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-md-offset-2">
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/5.png" />
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="text-left voffset3">
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Cũng ko cần giải thích nhiều thì các bạn cũng biết nhân viên trong công ty chúng tôi chủ yếu là người Việt Nam. Tại Việt Nam, bóng đá được mệnh danh là môn thể thao Vua, những chiến thắng của đội tuyển Việt Nam cũng là niềm tự hào của người Việt. Và chúng tôi trích một phần lợi nhuận của công ty - cũng là thành quả có được từ sự nỗ lực cố gắng của nhân viên để góp phần vào cổ vũ thắng lợi cho đội tuyển Việt Nam. Tức là , chính các bạn đang là một phần sức mạnh của đội tuyển Việt Nam - một trong những niềm tự hào của Quốc Gia. Giống như solgan của công ty chúng tôi "United we play, United we win" - chúng tôi sẽ cùng đồng hành với đội tuyển Việt Nam, đoàn kết và đi đến thắng lợi. </p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
             </div>
 
             <div class="row voffset3">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-md-offset-2">
 
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/8.png" class="text-colorimg" />
                     <h3 class="text-color"> <b> Tuyên ngôn quản lý</b></h3>
 
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="row voffset3">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-md-offset-2">
                     <img src="<?php echo get_template_directory_uri() ?>/img/vff/5.png" />
                 </div>
-                <div class="col-md-2"></div>
             </div>
 
             <div class="text-left voffset3">
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Việc trở thành nhà tài trợ cho và lớn mạnh cùng với đội tuyển Việt Nam là một trong những tuyên ngôn hướng ngoại về chiến lược kinh doanh của công ty.</p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
                         <p>Bằng việc đảm nhận trọng trách lớn là nhà tài trợ cho liên đoàn bóng đá Việt Nam , chúng tôi muốn thể hiện ý chí muốn mang đến sự phát triển hơn nữa cho Việt Nam thông qua sự nghiệp kinh doanh của mình. Toàn thể nhân viên công ty chúng tôi cùng với khách hàng sẽ góp phần vào sự phát triển đó. </p>
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
             </div>
 
